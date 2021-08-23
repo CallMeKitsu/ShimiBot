08a2aad25fbc048a37aab0f83111ae6407a9aa99
CallMeKitsu <kitsu.tnh.contact@gmail.com>
2021-08-23 10:51:47 +0200

    changement du BulkDelete

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -3,12 +3,12 @@ module.exports.run = async (client, message, args) => {
 
         if (message.member.hasPermission('MANAGE_MESSAGES')) {
             
-            let DeletedNumber = Math.floor(args[1])
+            let DeletedNumber = Math.floor(args[1]) + 1
 
             if (DeletedNumber) {
                 if (!isNaN(DeletedNumber) && DeletedNumber >= 1 && DeletedNumber <= 99) {
                     message.channel.bulkDelete(DeletedNumber)
-                    message.channel.send(`Vous avez supprimé ${DeletedNumber} message(s)`)
+                    message.channel.send(`Vous avez supprimé ${DeletedNumber - 1} message(s)`)
                 }
                 else {
                     message.channel.send(`Vous devez indiquer une valeur entre 1 et 99 !`)
