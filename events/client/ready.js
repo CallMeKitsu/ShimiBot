c41b4a06eb8ed8e40200ed03796e545c4fab0a8b
CallMeKitsu <kitsu.tnh.contact@gmail.com>
2021-09-25 17:46:08 +0200

    ajout des exports de stats

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js")
+const fs = require('fs')
 module.exports = (client, message) => {
 
   console.log(`Bot connecté sur ${client.guilds.cache.size} serveurs, pour un total de ${client.users.cache.size} utilisateurs.`);
@@ -15,7 +16,26 @@ module.exports = (client, message) => {
 
     client.user.setActivity(rstatus, { type: 'WATCHING'})
     
-  }, 3000);
+  }, 3000)
+
+  setInterval(() => {
+
+    let StableArray = client.commands.filter(stb => stb.config.stable === "✅ stable").map(cmd => cmd.config.name)
+
+    let JSONdata = JSON.parse(fs.readFileSync('../../html/shimi.web/ress/sample.json'))
+    const fileName = '../../html/shimi.web/ress/sample.json'
+
+    JSONdata.servers = client.guilds.cache.size
+    JSONdata.users = client.users.cache.size
+    JSONdata.channels = client.channels.cache.size
+    JSONdata.commands = StableArray.length
+        
+    fs.writeFile(fileName, JSON.stringify(JSONdata, null, 4), function writeJSON(err) {
+      if (err) return console.log(err);
+    });
+    
+  }, 60000)
+
   
 
 }
