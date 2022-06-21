32114bab9bcc5274f218d348a4f19f5cb5857f56
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-06-21 17:57:46 +0200

    help command

--- commands/admin/help.js
+++ commands/admin/help.js
@@ -6,20 +6,31 @@ const { MessageEmbed } = require('discord.js')
 module.exports.command = new CMDhandler(
   (interaction, config, client) => {
 
-    let parsed_help = ''
+    const embed = new MessageEmbed()
+      .setColor(config.EmColor)
+      .setThumbnail(client.user.avatarURL())
+      .setDescription(`\`\`\`/help <command_name>\`\`\``)
+
+    let emoji_index = 0
 
     client.COMMANDS.forEach( (catMap, catName) => {
-        parsed_help += `${catName} :\n`
-        catMap.forEach( (CMDhandler, cmdName) => {
-            parsed_help += ` --- ${cmdName}\n`
-        })
+
+      let catCommandsList = []
+      
+      catMap.forEach( (x, cmdName) => {
+        catCommandsList.push(cmdName)
+      })
+
+      catName = `\\${config.CatEmoji[emoji_index]} ${catName[0].toUpperCase() + catName.substring(1)}`
+      
+      if(catCommandsList.length > 0) embed.addField(catName, catCommandsList.join(', '))
+
+      emoji_index ++
     });
 
-    const embed = new MessageEmbed()
-        .setTitle('TEMP_HELP--DEV')
-        .setDescription(parsed_help)
 
-    interaction.reply({embeds: embed})
+
+    interaction.reply({embeds: [embed]})
     
   },
   new CMDstructure('help', "Affiche l'aide du bot !")
